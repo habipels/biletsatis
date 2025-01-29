cad6719fc0c38f4e9e3b39b61ed660e6e8de4545
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2025-01-29 17:56:10 +0300

    #genel güncelleme

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("register", views.register, name="register"),
-    path('login', views.custom_login, name='login'),
-    path('logout', views.custom_logout, name='logout'),
-    path('profile/<username>', views.profile, name='profile'),
-]
