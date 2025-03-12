d76c14dcc9b4d92585cbff070b2c9a04b687a05f
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2025-03-12 11:24:09 +0300

    #ufak güncellemeler

--- djang_website/urls.py
+++ djang_website/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = main_views.custom_404
 handler500 = main_views.custom_500
